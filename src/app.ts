5ad148043c44ffa138460ea47a93440a64b4acf0
mukhammat <dosnet2200@gmail.com>
2025-01-21 08:17:02 +0300

    Add PostgreSQL dependency and update app initialization to use Sequelize

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,21 @@
 import express from "express";
 const app = express();
 import {config} from "dotenv"; config();
+import {sequelize} from "./config/sequelize";
 
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || "http://localhost";
 
 console.log(process.env.PORT);
 
-app.listen(3000, (err) => {
-    if (err) {
-        return console.error(err);
+const start = async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        app.listen(PORT, () => console.log(`Server is running on ${HOST}:${PORT}`));
+    } catch (err) {
+        console.error(err);
     }
-    return console.log(`Server is running on ${HOST}:${PORT}`);
-});
+};
+
+start();
