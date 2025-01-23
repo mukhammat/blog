ca39f65af0920fd891a60f53ad3d577bf617428c
mukhammat <dosnet2200@gmail.com>
2025-01-23 14:50:29 +0300

    Add CORS support and update package dependencies

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import express from "express"; const app = express();
 import router from "./routers";
 import {setupSwagger} from "./utils/swagger";
 import { config } from "dotenv"; config();
+import cors from "cors"
 
 app.use(express.json());
 app.use("/api", router);
 setupSwagger(app);
+app.use(cors());
 
 
 const PORT = 3000;
